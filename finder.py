70ba0c5f24b3364ebc7e45832eff93ff446f0d60
dannydao <dannyhoangdao@gmail.com>
2023-11-27 17:05:03 -0600

    changed pop up messages to display within gui window instead

--- finder.py
+++ finder.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox
 from datetime import datetime
 import holidays
 
@@ -17,13 +16,13 @@ def check_holiday():
         # Validating if the date entered is a US holiday
         if input_date in us_holidays:
             # If the date entered is a holiday, this pop up message will appear
-            messagebox.showinfo("This date is a holiday", f"Holiday: {us_holidays[input_date]}")
+            holiday_msg.set(f"This date is a holiday: {us_holidays[input_date]}")
         else:
             # If the date entered is not a holiday, this pop up message will appear
-            messagebox.showinfo("This date is not a holiday", "Try a different date")
+            holiday_msg.set("This date is not a holiday. Try a different date")
     except ValueError:
         # Invalid date entries will return this popup message
-        messagebox.showerror("Invalid Date", "Enter a valid date (Format: MM/DD)")
+        holiday_msg.set("Enter a valid date (Format: MM/DD)")
 
 
 # Creating GUI
@@ -34,8 +33,9 @@ window.geometry("275x150")
 # Sets window to non-resizables
 window.resizable(False, False)
 
-# StringVar to hold the date input
+# StringVar to hold the date input and message result
 date_var = tk.StringVar()
+holiday_msg = tk.StringVar()
 
 # Label telling user to enter a date
 date_label = tk.Label(window, text="Enter date (MM/DD): ")
@@ -45,10 +45,14 @@ entry1 = tk.Entry(window, textvariable=date_var)
 # Button to check the input for US holidays
 check_button = tk.Button(window, text="Is it a holiday?", command=check_holiday)
 
+# Label to display holiday result
+result = tk.Label(window, textvariable=holiday_msg, wraplength=250)
+
 # Placing and positioning for labels, entry field, and button using grid layout
-date_label.grid(row=0,column=0,pady=15,padx=7)
-entry1.grid(row=0, column=1,pady=15)
-check_button.grid(row=2,column=0,columnspan=2,pady=15)
+date_label.grid(row=0, column=0, pady=15, padx=7)
+entry1.grid(row=0, column=1, pady=15)
+check_button.grid(row=2, column=0, columnspan=2, pady=15)
+result.grid(row=3, column=0, columnspan=2, pady=10)
 
 # Run the main event loop
 window.mainloop()
